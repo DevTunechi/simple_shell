f41238038c66462c25f7410d43c06632c3e1bc52
goodnewsakpan <goodnewsakpan123@gmail.com>
2023-12-13 07:48:02 +0100

    new corrections

--- errors_sh.c
+++ errors_sh.c
@@ -5,22 +5,23 @@
 * @c: char to print
 * @fd: filedescriptor to write to
 * Olatunji and Goodnews
-* Return: 1, on failure -1 and errno will set approrpiately */
+* Return: 1, on failure -1 and errno will set appropiately
+*/
 
 int _putfd(char c, int fd)
 {
 	static int i;
 	static char buf[WRITE_BUF_SIZE];
-	
+
 	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
 	{
 		write(fd, buf, i);
 		i = 0;
 	}
-	
+
 	if (c != BUF_FLUSH)
 		buf[i++] = c;
-	
+
 	return (1);
 }
 
@@ -31,18 +32,17 @@ int _putfd(char c, int fd)
  * Olatunji and Goodnews
  * Return: num of chars to put
  */
- 
- int _putsfd(char *str, int fd)
- {
-	 int i = 0;
-	 
-	 if (!str)
-		 
-		 return (0);
-	 while (*str)
-	 {
-		 i += _putfd(*str++, fd);
-	 }
-	 
-	 return (i);
- }
+
+int _putsfd(char *str, int fd)
+{
+int i = 0;
+
+if (!str)
+return (0);
+while (*str)
+{
+i += _putfd(*str++, fd);
+}
+
+return (i);
+}
